68f0bf9671ad258e43cebc6141cd5510f3a3e8ce
rinoguchi <inogu1979@gmail.com>
2021-05-31 18:03:31 +0900

    comment add and get function

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,63 @@
 import express from 'express';
+import * as mariadb from 'mariadb';
 
+// DB コネクションプールの初期化
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 5
+});
+
+// express 初期化
 const app = express();
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!!');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+// ルーティングの定義
+app.get('/comments/:id', async (req: express.Request, res: express.Response) => {
+    let conn: mariadb.PoolConnection | undefined;
+    try {
+        conn = await pool.getConnection();
+        const comment: Comment | null = await getComment(conn, Number(req.params.id));
+        res.send(comment);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.release();
+    }
+})
+app.post('/comments', async (req: express.Request, res: express.Response) => {
+    let conn: mariadb.PoolConnection | undefined;
+    try {
+        conn = await pool.getConnection();
+        const id: number = await insertComment(conn, req.body.comment);
+        const comment: Comment | null = await getComment(conn, id);
+        res.send(comment);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) conn.release();
+    }
 })
 app.listen(3000);
+
+// モデル定義
+type Comment = {
+    id: number;
+    comment: string;
+}
+
+// DB アクセス
+const getComment = async (conn: mariadb.PoolConnection, id: number): Promise<Comment | null> => {
+    const res: Array<Comment> = await conn.query("select * from comments where id = ?", [id]);
+    if (res.length === 0) return null
+    return res[0];
+}
+const insertComment = async (conn: mariadb.PoolConnection, comment: string): Promise<number> => {
+    const res = await conn.query("insert into comments(comment) value (?)", [comment]);
+    const id = res.insertId;
+    return id;
+}
